057e5476731d5488b97ad74f0022389fd55c448a
Mhammed-Tabarani <mhammed.tabarani@gmail.com>
2021-03-12 15:07:51 +0000

    Ajoutez des fichiers projet.

---
+++ Atelier/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atelier
+{
+    class Client
+    {
+        private string Nom, Prenom, Adresse;
+        public Client(string Nom, string Prenom, string Adresse)
+        {
+            this.Nom = Nom;
+            this.Prenom = Prenom;
+            this.Adresse = Adresse;
+        }
+        public void afficher()
+        {
+            Console.WriteLine("Nom = " + this.Nom + "\n Prenom = " + this.Prenom + "\n Adresse = " + this.Adresse);
+        }
+
+    }
+}
---
+++ Atelier/Mad.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atelier
+{
+    class Mad
+    {
+        private double valeur;
+
+        public Mad(double V)
+        {
+            this.valeur = V;
+        }
+        public void afficher()
+        {
+            Console.WriteLine(this.valeur);
+        }
+        public bool ispositif()
+        {
+            if (this.valeur > 0)
+                return (true);
+            return false;
+        }
+        public static Mad operator +(Mad V1, Mad V2)
+        {
+            Mad Somme = new Mad(V1.valeur + V2.valeur);
+            return (Somme);
+        }
+        public static Mad operator -(Mad V1, Mad V2)
+        {
+            Mad Sous = new Mad(V1.valeur - V2.valeur);
+            return (Sous);
+        }
+        public static bool operator <=(Mad V1, Mad V2)
+        {
+            if (V1.valeur <= V2.valeur)
+                return (true);
+            return false;
+        }
+        public static bool operator >=(Mad V1, Mad V2)
+        {
+            if (V1.valeur >= V2.valeur)
+                return (true);
+            return false;
+        }
+        public static bool operator <(Mad V1, Mad V2)
+        {
+            if (V1.valeur < V2.valeur)
+                return (true);
+            return false;
+        }
+        public static bool operator >(Mad V1, Mad V2)
+        {
+            if (V1.valeur > V2.valeur)
+                return (true);
+            return false;
+        }
+    }
+}
+
